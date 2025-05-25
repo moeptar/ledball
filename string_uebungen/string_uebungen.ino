673520c5a087c867840226e91a3ac723d283d9b1
Michael Jackes <michael.j@gmx.de>
2025-05-25 15:36:25 +0200

    merkwürdiger zwischenstand

--- string_uebungen/string_uebungen.ino
+++ string_uebungen/string_uebungen.ino
@@ -3,14 +3,14 @@
 // C -> Warte auf Catch -> C
 // T -> Warte auf Wurf -> T
 
-String testbefehl1 = "LFF000000FF00";
-String testbefehl2 = "D000000000500";
-String testbefehl3 = "L00FF000000FF";
-String testbefehl4 = "D000000001000";
-String testbefehl5 = "L0000FFFF0000";
-String testbefehl6 = "D000000001500";
+String testbefehl1 = "LFF000000FF00;";
+String testbefehl2 = "D1000;";
+String testbefehl3 = "L00FF000000FF;";
+String testbefehl4 = "D2000;";
+String testbefehl5 = "L0000FFFF0000;";
+String testbefehl6 = "D1500;";
 
-String testprogramm = testbefehl1 + ";" + testbefehl2 + ";" + testbefehl3 + ";" + testbefehl4 + ";" + testbefehl5 + ";" + testbefehl6 + ";";
+String testprogramm = testbefehl1 + testbefehl2 + testbefehl3 + testbefehl4 + testbefehl5 + testbefehl6;
 
 class Command {
   private:
@@ -20,11 +20,15 @@ class Command {
     String duration;
 
   public:
-    Command(String commandString) {
+    void setValue(String commandString) {
       type = commandString.substring(0,1);
-      color0 = commandString.substring(1,7);
-      color1 = commandString.substring(8,14);
-      duration = commandString.substring(1,14);
+      if(type == "L"){
+        color0 = commandString.substring(1,7);
+        color1 = commandString.substring(7,13);
+      }
+      if(type == "D"){
+        duration = commandString.substring(1,commandString.length());
+      }
     }
 
     void print() {
@@ -37,7 +41,7 @@ class Command {
         Serial.print("Set LED 1 to (");
         Serial.print(color0.substring(0,2));
         Serial.print(",");
-        Serial.print(color0.substring(3,5));
+        Serial.print(color0.substring(2,4));
         Serial.print(",");
         Serial.print(color0.substring(4,6));
         Serial.print(")");
@@ -46,7 +50,7 @@ class Command {
         Serial.print("Set LED 2 to (");
         Serial.print(color1.substring(0,2));
         Serial.print(",");
-        Serial.print(color1.substring(3,5));
+        Serial.print(color1.substring(2,4));
         Serial.print(",");
         Serial.print(color1.substring(4,6));
         Serial.print(")");
@@ -57,6 +61,10 @@ class Command {
         Serial.print(duration);
         Serial.print(" ms");
         Serial.println();
+
+        Serial.print("Waiting");
+        delay(atoi(duration.c_str()));
+        Serial.println();
       }
 
     }
@@ -64,47 +72,55 @@ class Command {
 
 class Program {
   private:
-    int numberCommands = 0;
-    int idxCommand = 0;
-    String programString = "LB0B0B0";
+    int idxProgramString = 0;
+    String programString = "LB0B0B0;";
 
   public:
-
     void setProgramString(String program) {
       programString = program;
-      for (uint8_t i=0; i < programString.length(); i++){
-        if (programString[i] == ';'){
-          numberCommands++;
-        }
-      }
-      Serial.print("Program ");
-      Serial.println(programString);
-      Serial.print("-> ");
-      Serial.print(numberCommands);
-      Serial.println(" commands");    }
+      idxProgramString = 0;
+    }
 
+    String nextStep(){
+      String restString = programString.substring(idxProgramString);
+      Serial.println(restString);
+      if(restString.indexOf(';') == -1){
+        Serial.println("return to zero");
+        idxProgramString = 0;
+      } else {
+        Serial.println(restString.indexOf(";"));
+        idxProgramString = restString.indexOf(";") + 1;
+      }
+      return restString.substring(idxProgramString, restString.indexOf(";"));
+    }
 };
 
-Command currendCommand("D1000;");
+Command currendCommand;
 Program currendProgram;
 
 void setup() {
     Serial.begin(115200);
-    Serial.println(testprogramm);
-    currendCommand = Command("D1000");
+    currendCommand.setValue("D1;");
     currendProgram.setProgramString(testprogramm);
 }
+int idx;
+int nextIdx;
 
 void loop() {
-  currendCommand.execute();
-  currendCommand = testbefehl1;
-  currendCommand.execute();
-  currendCommand = testbefehl2;
-  currendCommand.execute();
-  currendCommand = testbefehl3;
-  currendCommand.execute();
-  currendCommand = testbefehl4;
-  currendCommand.execute();
-  delay(1000);
+//  String command = currendProgram.nextStep();
+//  Serial.println("->" + command);
+//  currendCommand.setValue(command);
+  
+  nextIdx = idx + testprogramm.substring(idx).indexOf(";") + 1;
+  Serial.println(idx);
+  Serial.println(nextIdx);
+  Serial.println(testprogramm);
+  Serial.println(testprogramm.substring(nextIdx));
 
+  idx = nextIdx;
+  if(idx > testprogramm.length()){
+    idx = 0;
+  }
+
+  delay(1000);
 }
